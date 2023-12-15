167e5bcbfc69a159cc06d8cd4b4395134bdf7e31
himanshusinha <himanshusinha011@gmail.com>
2023-12-15 21:31:19 +0530

    code correction email

---
+++ data/database.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+export const connectDB = async () => {
+  try {
+    const { connection } = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "EShop",
+    });
+    console.log(`Server connected to database ${connection.host}`);
+  } catch (error) {
+    console.log("Some Error Occurred", error);
+    process.exit(1);
+  }
+};
