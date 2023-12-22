b0a6bfbda53f0e7b96912bc975fa9124b2d336f6
himanshusinha <himanshusinha011@gmail.com>
2023-12-22 07:51:04 +0530

    orders

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,22 @@
+import { User } from "../models/user.js";
 import ErrorHandler from "../utils/error.js";
 import jwt from "jsonwebtoken";
 import { asyncError } from "./error.js";
-export const isAuthenticated = (req, res, next) => {
-  const token = req.header("Authorization");
 
-  if (!token) {
-    return res.status(401).json({ message: "No token provided" });
-  }
+export const isAuthenticated = asyncError(async (req, res, next) => {
+  // const token = req.cookies.token;
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user; // Attach user info to the request
-    next();
-  } catch (error) {
-    res.status(401).json({ message: "Token is not valid" });
-  }
-};
+  const { token } = req.cookies;
+
+  if (!token) return next(new ErrorHandler("Not Logged In", 401));
+
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+  req.user = await User.findById(decodedData._id);
+
+  next();
+});
+
 export const isAdmin = asyncError(async (req, res, next) => {
   if (req.user.role !== "admin")
     return next(new ErrorHandler("Only Admin allowed", 401));
